bce7d2bd40cbfde4b0339bcbe128eeab5a8cef39
“000000jerry” <761797477@qq.com>
2015-01-15 16:37:19 +0800

    Initial Commit

---
+++ iOSFunctionalityUsingDemos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSFunctionalityUsingDemos
+//
+//  Created by 刘杰cjs on 15/1/15.
+//  Copyright (c) 2015年 com.cjs.ljc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSFunctionalityUsingDemos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSFunctionalityUsingDemos
+//
+//  Created by 刘杰cjs on 15/1/15.
+//  Copyright (c) 2015年 com.cjs.ljc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
